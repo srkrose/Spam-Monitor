be44d392f1fc6d294057b41e727b93c648857e26
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2023-12-15 18:22:46 +0530

    Create dataset.sh

---
+++ dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cpuser="sample"
+scripts="/home/$cpuser/scripts"
+svrlogs="/home/$cpuser/svrlogs"
+temp="$svrlogs/temp"
+hostname=$(hostname | awk -F'.' '{print $1}')
+svrdomain=$(hostname)
+svrip=$(dig $(hostname) A +short)
+logtime=$(date +"%Y-%m")
+logdate=$(date +"%Y-%m-%d")
+time=$(date +"%F_%T")
+date=$(date +"%F")
+validation="..."
+emailmo="test1@domain.com"
+emailmg="test2@domain.com"
+spamemailslack="https://hooks.slack.com/..."
