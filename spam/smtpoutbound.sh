a301f3ace8f5a8e87c1b0a08f6c0073eef4190ab
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-03-21 11:56:52 +0530

    Update smtpoutbound.sh

--- spam/smtpoutbound.sh
+++ spam/smtpoutbound.sh
@@ -78,7 +78,7 @@ function notify() {
 function send_sms() {
 	message=$(echo "$hostname: $content")
 
-	php $scripts/send_sms.php "$message" "$validation"
+	#php $scripts/send_sms.php "$message" "$validation"
 
 	curl -X POST -H "Content-type: application/json" --data "{\"text\":\"$message\"}" $spamemailslack
 }
