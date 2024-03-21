6a64b1a143d44d63f85c59df5ccbc821d6419ea1
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-03-21 11:56:08 +0530

    Update limitexceeded.sh

--- spam/limitexceeded.sh
+++ spam/limitexceeded.sh
@@ -87,7 +87,7 @@ function suspend_user() {
 function send_sms() {
 	message=$(echo "$hostname: $content")
 
-	php $scripts/send_sms.php "$message" "$validation"
+	#php $scripts/send_sms.php "$message" "$validation"
 
 	curl -X POST -H "Content-type: application/json" --data "{\"text\":\"$message\"}" $spamemailslack
 }
